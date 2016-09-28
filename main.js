50a128b852a40223f5079c9070ec4ecb92fc1ff9
Milad <Milad@Milads-MacBook-Pro.local>
2016-09-28 02:09:46 -0700

    button click not working properly

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 
 
+
+
 function getSubreddits() {
-	var subreddit = document.getElementById('subreddit').textContent;
+	var subreddit = document.getElementById('subreddit').value;
 	console.log(subreddit);
-	$.ajax({
+	var promise = $.ajax({
 		url: 'https://www.reddit.com/r/'+subreddit+'.json',
-		success: function(response) {
+		type: 'get'
+	}).then(function(response) {
 			var templateSource = $('#song-list-template').html();
 			var template = Handlebars.compile(templateSource);
 
-			var html = template(response); 
+			var html = template(response.data); 
 			console.log(response);
+			console.log(response.data.children)
 			$('#song-list').html(html);
-		}
+	}, function() {
+		console.log('EROROR');
+		//document.getElementById('error').innerHTML = "THere was an error, nothing found!"
 	});
 }
+
+$('#song-list').on('click','button',function() {
+	var selftext = $(this).parent().parent().find('selftext').html();
+	console.log(selftext);
+	alert($(this).find('selftext').html());
+});
